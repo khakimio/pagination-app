73f4dd7262dfb6af384567be554384fde2533579
Khakim <khakimme@gmail.com>
2024-10-06 14:49:13 +0500

    refactor: separate svg icons

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -1,4 +1,10 @@
 import usePagination from '../../hooks/usePagination'
+import {
+  LeftArrowIcon,
+  LeftAnglesIcon,
+  RightArrowIcon,
+  RightAnglesIcon,
+} from '../Icons/Arrows'
 import './Pagination.scss'
 
 type Props = {
@@ -120,50 +126,3 @@ function PaginationButton({
     </button>
   )
 }
-
-const LeftArrowIcon = () => (
-  <svg
-    width="16"
-    height="16"
-    fill="currentColor"
-    viewBox="0 0 320 512"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z" />
-  </svg>
-)
-
-const LeftAnglesIcon = () => (
-  <svg
-    width="22"
-    height="22"
-    fill="currentColor"
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 512 512"
-  >
-    <path d="M41.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.3 256 246.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160zm352-160l-160 160c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L301.3 256 438.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0z" />
-  </svg>
-)
-
-const RightArrowIcon = () => (
-  <svg
-    width="16"
-    height="16"
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 320 512"
-  >
-    <path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z" />
-  </svg>
-)
-
-const RightAnglesIcon = () => (
-  <svg
-    width="22"
-    height="22"
-    fill="currentColor"
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 512 512"
-  >
-    <path d="M470.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L402.7 256 265.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160zm-352 160l160-160c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L210.7 256 73.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0z" />
-  </svg>
-)
